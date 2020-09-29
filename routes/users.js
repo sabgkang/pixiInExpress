169dc2dc239618bd37c00abf6f3dc2a1f7d6cc34
Paul Kang <paul@paul-kang.com>
2020-09-29 22:01:16 +0800

    add using nodejs as GCI

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  console.log('respond with a resource');
   res.send('respond with a resource');
 });
 
