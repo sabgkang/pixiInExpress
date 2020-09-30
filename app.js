92213f5aaa6cf2bc84f5cbd98a854f58152a6d17
paulchainkang <paul@paul-kang.com>
2020-09-30 18:16:14 +0800

    reduce one layer

--- app.js
+++ app.js
@@ -19,32 +19,32 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.get('/', (req, res) => {
-//  res.render('spine');
-//})
+app.get('/', (req, res) => {
+  res.render('spine');
+})
+
+app.get('/users/', (req, res) => {
+  console.log(req.query); //不是 params
+  res.send('users');
+})
+
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
 //
-//app.get('/users/', (req, res) => {
-//  console.log(req.query); //不是 params
-//  res.send('users');
-//})
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//// catch 404 and forward to error handler
+//app.use(function(req, res, next) {
+//  next(createError(404));
+//});
+//
+//// error handler
+//app.use(function(err, req, res, next) {
+//  // set locals, only providing error in development
+//  res.locals.message = err.message;
+//  res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//  // render the error page
+//  res.status(err.status || 500);
+//  res.render('error');
+//});
 
 module.exports = app;
